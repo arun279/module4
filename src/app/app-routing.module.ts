26be0f41d5b45262dacb30e872a317c58bad8ac9
Arun Krishnamurthy <infinite279@gmail.com>
2019-12-12 14:53:30 -0500

    completed basic login format, but routing still doesn't work
    <app-login> not added to app.component therefore nothing shows up in home page. until routing is completed, localhost will be blank unless app-login tag explicitly added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,8 @@ import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'login', component: LoginComponent },
-  { path: '', component: LoginComponent }
+  { path: 'login', component: LoginComponent }
+  // { path: '', component: LoginComponent }
 ];
 
 @NgModule({
